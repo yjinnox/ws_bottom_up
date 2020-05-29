721ed26483018d6e8f66f5c3561cc4f7517a80b7
yjinnox <yjinnox@hotmail.com>
2020-05-29 04:33:52 +0200

    first commit on the project ws_bottom

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+_
+/src/main/java/com/lifeleft/LifeleftService.java,f\1\f1a001fe7b3ad4287390b8a1530c73c8aa8ec2d7
---
+++ src/main/java/com/lifeleft/LifeleftService.java
@@ -0,0 +1,43 @@
+package com.lifeleft;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import java.time.Year;
+import java.util.concurrent.ThreadLocalRandom;
+
+@WebService(serviceName = "LifeLeft")
+public class LifeleftService {
+    private static  final Integer ESPERANCE_VIE_HOMMES = 79;
+    private static  final Integer ESPERANCE_VIE_FEMMES = 85;
+
+    String homme = "HOMME";
+    String femme = "FEMME";
+
+    Integer evDeReference = 0;
+
+    @WebMethod
+    public  String anneesRestantesAVivre(String prenom, String sexe,Integer anneeNaissance){
+        if(sexe.equals(homme)) {
+            evDeReference = ESPERANCE_VIE_HOMMES;
+        } else evDeReference = ESPERANCE_VIE_FEMMES;
+
+        Integer anneesRestantes = evDeReference - (Year.now().getValue() - anneeNaissance);
+
+        String valeurDeRetour = "Bonjour "+prenom+" il vous reste à vivre "+anneesRestantes+ "année";
+
+        return  valeurDeRetour;
+    }
+
+    @WebMethod
+    public int creerClient(String login, String password){
+
+        return ThreadLocalRandom.current().nextInt(100, 900);
+    }
+
+    @WebMethod
+    public String commanderCompteARebour(Integer clientId) {
+
+        return "Merci ! Votre commande pour le client: "+ clientId + "de compteur de vie est validée";
+
+    }
+}
